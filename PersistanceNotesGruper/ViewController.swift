d4aefc87d678b0cf903a33445e6429c30b3e5080
Michael Gruper <sarge43210@gmail.com>
2024-10-16 07:30:11 -0500

    Initial Commit

---
+++ PersistanceNotesGruper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersistanceNotesGruper
+//
+//  Created by MICHAEL GRUPER on 10/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PersistanceNotesGruperTests/PersistanceNotesGruperTests.swift
@@ -0,0 +1,17 @@
+//
+//  PersistanceNotesGruperTests.swift
+//  PersistanceNotesGruperTests
+//
+//  Created by MICHAEL GRUPER on 10/16/24.
+//
+
+import Testing
+@testable import PersistanceNotesGruper
+
+struct PersistanceNotesGruperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
